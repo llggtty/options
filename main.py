b281019277fa5073db8e50e0b760fcfe6852785f
llggtty <junhuang@gmail.com>
2021-06-12 23:17:01 +0200

    update

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 #!/usr/bin/venv python3.7
-import os
-import math
-import numpy as np
-import pandas as pd
 from options.utils.dgp import *
 from options.common.config import *
 pd.options.display.float_format = '{:,.3f}'.format
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.max_rows', 2000)
 
-import logging
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s')
-
-
 
 if __name__ == '__main__':
     pricing_data = generate_pricing_data(initial_price, annual_return, annual_volatility, days_to_expiry)
-    rvol = analytical_realised_volatility(pricing_data['base_price'])
+    rvol = realised_volatility(pricing_data['base_price'])
+    # if realised vol > implied vol, portfolio pnl > 0.
     pnl = portfolio_pnl(pricing_data)
 
     print(pnl, rvol, annual_volatility)
--- utils/dgp.py
+++ utils/dgp.py
@@ -1,26 +1,27 @@
 #!/usr/bin/venv python3.7
 import numpy as np
 import pandas as pd
+from typing import Optional
 from options.common.config import *
 from options.core.bs_model import BlackScholesModel as bs, Option
 pd.options.display.float_format = '{:,.3f}'.format
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.max_rows', 2000)
 
-import logging
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s')
 
-
-def generate_base_prices(p0: float, annual_return: float, volatility: float, trading_days: int) -> np.ndarray:
+def generate_base_prices(p0: float, annual_return: float, volatility: float, trading_days: int,
+                         seed: Optional[int] = None) -> np.ndarray:
     """
     first generate a return series following normal distribution
     :param p0: price at time0
     :param annual_return: expected annual return
     :param volatility: expected annual volatility
-    :param trading_days: int
+    :param trading_days: how many data points to generate
+    :param seed: whether set a seed
     :return: series of daily price
     """
-    np.random.seed(100)
+    if seed:
+        np.random.seed(seed)
     sigma = volatility / math.sqrt(TRADING_DAYS)
     mu = annual_return / TRADING_DAYS
     # return from day 1
@@ -30,12 +31,12 @@ def generate_base_prices(p0: float, annual_return: float, volatility: float, tra
     return np.insert(prices, [0], p0)
 
 
-def generate_pricing_data(p0: float, annual_return: float, volatility: float, days_to_expiry: int) -> pd.DataFrame:
+def generate_pricing_data(p0: float, annual_return: float, volatility: float, days_to_expiry: int, seed: Optional[int] = None) -> pd.DataFrame:
     """
     generate option pricing data based on configured pricing inputs
     :return: dataframe of base price, tte and the corresponding option price, delta
     """
-    arr_base = generate_base_prices(p0, annual_return, volatility, days_to_expiry)
+    arr_base = generate_base_prices(p0, annual_return, volatility, days_to_expiry, seed)
     # including day 0
     arr_tte = [(days_to_expiry - x) / days_to_expiry for x in np.arange(0, days_to_expiry+1)]
     pricing_data = pd.DataFrame(data={'base_price': arr_base, 'tte': arr_tte})
@@ -46,42 +47,29 @@ def generate_pricing_data(p0: float, annual_return: float, volatility: float, da
     return pricing_data
 
 
-def portfolio_pnl(pricing_data: pd.DataFrame) -> float:
+def portfolio_pnl(pricing_data: pd.DataFrame, rounding: bool) -> tuple[float, pd.DataFrame]:
     """
     :param pricing_data: dataframe of base price, tte and the corresponding option price, delta
+    :param rounding: if set to true, can only hedge integer stocks
     :return: dataframe of # of stocks for hedge and the pnl.
     """
     last_row = len(pricing_data) - 1
-    # hedge can only be integer
-    pricing_data['hedge'] = -pricing_data['delta'] * contract_multiplier
+    pricing_data['hedge'] = -1 * pricing_data['delta'] * contract_multiplier
+    if rounding:
+        # hedge can only be integer
+        pricing_data['hedge'] = pricing_data['hedge'].round()
     pricing_data['hedge_volume'] = pricing_data['hedge'] - pricing_data['hedge'].shift(1)
     # the first trade is the hedge amount
     pricing_data.loc[0, 'hedge_volume'] = pricing_data.loc[0, 'hedge']
 
     pricing_data['hedge_pnl'] = pricing_data['hedge_volume'] * (pricing_data.loc[last_row, 'base_price']-pricing_data['base_price'])
+    hedge_pnl = pricing_data['hedge_pnl'].sum()
+
     option_pnl = (pricing_data.loc[last_row, 'option_price'] - pricing_data.loc[0, 'option_price']) * contract_multiplier
-    final_pnl = pricing_data['hedge_pnl'].sum() + option_pnl
-    return final_pnl
+    return hedge_pnl + option_pnl, pricing_data
 
 
-
-def realised_volatility(p: pd.Series) ->float:
-    """
-    :param p: price series
-    :return: annualised realised volatility
-    """
-    t = len(p)
-    def gamma_pnl(ds, gamma):
-        returns = ds / ds.iloc[0]
-        d = returns.diff().iloc[1:]
-        d = d.apply(lambda v: gamma * 0.5 * v**2)
-        return d.cumsum()
-
-    def pnl_to_vol(pnl, gamma, t):
-        return math.sqrt(2*pnl/(gamma * t/TRADING_DAYS))
-
-
-def analytical_realised_volatility(p: pd.Series) -> float:
+def realised_volatility(p: pd.Series) -> float:
     """
     :param p: price series
     :return: annualised realised volatility
